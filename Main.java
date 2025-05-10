bd9cfc8ba68ffc312765568ec979a130de6dfda1
RahmaOsamaMohamed <162125852+RahmaOsamaMohamed@users.noreply.github.com>
2025-05-10 17:26:37 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 import Authentication.AuthService;
-
 public class Main {
     public static void main(String[] args) {
         AuthService authService = new AuthService();
         ConsoleUI ui = new ConsoleUI(authService);
         ui.start();
+
     }
 }
